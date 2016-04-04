4bf50981e8a5afb033366fae17891660e9b4eef2
Adilf <Adilf@users.noreply.github.com>
2016-04-05 00:57:07 +0300

    Multiple gui support
    It seems to me that these changes allow the WrenchFu to be used by several modules at the same time and not have unnessessary clashes. 
    Before, the mod would close all other guis opened with it. yet since there are no specifications on what is drawn by caller mod, this may be unwise.
    Now mods should explicitly provide the list of entities handlers they wish not to be openned together. 
    Also the max_distance  setting was changed to be mod specific

--- WrenchFu/registry.lua
+++ WrenchFu/registry.lua
@@ -5,19 +5,26 @@ WrenchFu's registry: keeps the wrenchable handler registry.
 if not WrenchFu then WrenchFu = {} end
 local handler_registry = {}
 
-
 interface = {}
-function interface.register(entity_name, mod_name, show_method, hide_method)
+function interface.register(entity_name, mod_name, show_method, hide_method, max_distance,no_interlace_with)
     local handler = handler_registry[entity_name]
     if handler ~= nil and handler.mod_name ~= mod_name then
         WrenchFu.notify_all({"warn_stealing_registry", entity_name, handler.mod_name, mod_name})
     end
-
+    
+    no_interlace_with = no_interlace_with or {}
+    no_interlace_with.[entity_name]=true
+    
     handler_registry[entity_name] = {
         mod_name = mod_name,
         show_method = show_method,
         hide_method = hide_method,
+        max_distance = max_distance,
+        no_interlace_with = no_interlace_with 
     }
+    if max_distance and  ( ( not WrenchFu.max_distance) or WrenchFu.max_distance < max_distance ) then
+        WrenchFu.max_distance = max_distance
+    end
 end
 remote.add_interface("WrenchFu", interface)
 

fd0da1c1d5e9ed302707a52320e36b501d4d8f05
Adilf <Adilf@users.noreply.github.com>
2016-04-30 03:11:53 +0300

    Update registry.lua

--- WrenchFu/registry.lua
+++ WrenchFu/registry.lua
@@ -6,25 +6,30 @@ if not WrenchFu then WrenchFu = {} end
 local handler_registry = {}
 
 interface = {}
-function interface.register(entity_name, mod_name, show_method, hide_method, max_distance,no_interlace_with)
+function interface.register(entity_name, handler_params)
+    
     local handler = handler_registry[entity_name]
-    if handler ~= nil and handler.mod_name ~= mod_name then
-        WrenchFu.notify_all({"warn_stealing_registry", entity_name, handler.mod_name, mod_name})
+    if handler ~= nil and handler.mod_name ~= handler_params.mod_name then
+        WrenchFu.notify_all({"warn_stealing_registry", entity_name, handler.mod_name, handler_params.mod_name})
+    end
+    handler = handler_params
+    if not handler.mod_name or not remote.interfaces[handler.mod_name] then
+        WrenchFu.notify_all({"error_no_interface name", entity_name}) end
+    if not handler.show_method or not remote.interfaces[handler.mod_name][handler.show_method] then
+        WrenchFu.notify_all({"error_no_function_to_call", entity_name, handler_params.mod_name}) 
     end
     
-    no_interlace_with = no_interlace_with or {}
-    no_interlace_with[entity_name]=true
+    handler.no_interlace_with = handler.no_interlace_with or {}
+    handler.no_interlace_with[entity_name]=true
     
-    handler_registry[entity_name] = {
-        mod_name = mod_name,
-        show_method = show_method,
-        hide_method = hide_method,
-        max_distance = max_distance,
-        no_interlace_with = no_interlace_with 
-    }
+    handler_registry[entity_name] = handler
+    
     if max_distance and  ( ( not WrenchFu.max_distance) or WrenchFu.max_distance < max_distance ) then
         WrenchFu.max_distance = max_distance
     end
+end
+function interface.close(player_index,entity_name)
+    return WrenchFu.close_gui_unconditionally(player_index,entity_name)
 end
 remote.add_interface("WrenchFu", interface)
 

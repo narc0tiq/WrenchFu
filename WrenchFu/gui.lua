8a89a2a4487cf339c968fdd9fd54fc64bcce3197
Adilf <Adilf@users.noreply.github.com>
2016-05-01 00:30:11 +0300

    Update gui.lua
    Support for older interface added

--- WrenchFu/gui.lua
+++ WrenchFu/gui.lua
@@ -15,26 +15,30 @@ if not WrenchFu then WrenchFu = {} end
 
 function WrenchFu.open_gui_for(player_index, entity)
     local reg = WrenchFu.handler_for(entity.name)
-    
     global.open_guis[player_index] = global.open_guis[player_index] or {}
     
     global.open_guis[player_index][entity.name] = {
         ["position"] = entity.position,
         ["handler"] = reg,
     }
-
-    remote.call(reg.mod_name, reg.show_method, player_index, entity)
+    if not reg.legacy then
+        remote.call(reg.mod_name, reg.show_method, player_index, entity)
+    elseif reg.legacy=="0.1.2" then
+        remote.call(reg.mod_name, reg.show_method, player_index, entity.name, entity.position)
+    end
 end
 
 
 function WrenchFu.close_gui_for(player_index,entity_name)
-    if global.open_guis[player_index] == nil then return end
-    if global.open_guis[player_index][entity_name] == nil then return end
+    if global.open_guis[player_index] == nil then return true end
+    if global.open_guis[player_index][entity_name] == nil then return true end
     local gui_data = global.open_guis[player_index][entity_name]
 
     local reg = gui_data.handler
-    if remote.call(reg.mod_name, reg.hide_method, player_index, entity_name, gui_data.position) then
+    local permit= remote.call(reg.mod_name, reg.hide_method, player_index, entity_name, gui_data.position)
+    if permit or reg.legacy=="0.1.2" then
         global.open_guis[player_index][entity_name] = nil
+        return true
     end
 end
 
@@ -49,7 +53,6 @@ function WrenchFu.open_gui_at(place, player_index)
 
     local player = game.get_player(player_index)
     if not player then return end
-
     local ents = player.surface.find_entities({place, place})
     for _,v in pairs(ents) do
         if WrenchFu.handler_for(v.name) ~= nil then
@@ -67,7 +70,6 @@ end
 
 function WrenchFu.on_built_entity(event)
     local ent = event.created_entity
-
     local player = game.get_player(event.player_index)
     if ent.name == "wrenchfu-wrench" then
         if not player.cursor_stack.valid_for_read then
@@ -84,11 +86,10 @@ end
 function WrenchFu.on_tick(event)
     if WrenchFu.max_distance == nil then return end
     if not global.open_guis then global.open_guis = {} end
-
     for player_index, player in pairs(game.players) do
         if global.open_guis[player_index] then 
             for entity_name,gui_data in pairs(global.open_guis[player_index]) do
-                if not gui_data.handler.max_distance then 
+                if gui_data.handler.max_distance then 
                     if util.distance(player.position, gui_data.position) > gui_data.handler.max_distance then
                         WrenchFu.close_gui_for(player_index,entity_name)
                     end
